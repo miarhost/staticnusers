881641586441f7fa9af140200b08572af03f0558
MariaTikhonova <ritz.rkraft@gmail.com>
2017-11-08 20:21:17 +0200

    Add a static pages controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+  	render html: "Rghost is here, glory to holy regexp!"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'pages/main'
+  get 'pages/about'
+  root 'application#hello'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
